1ba83c4da22ebbe4285d7074d8f497b3ab44d8c6
podhmo <ababjam61+github@gmail.com>
2020-09-27 12:51:58 +0900

    feat: transport utility

---
+++ transport.go
@@ -0,0 +1,31 @@
+package reqtest
+
+import (
+	"net/http"
+	"net/http/httptest"
+)
+
+type RoundTripFunc func(*http.Request) (*http.Response, error)
+
+func (f RoundTripFunc) RoundTrip(r *http.Request) (*http.Response, error) {
+	return f(r)
+}
+
+type HandlerTripper struct {
+	Before  func(*http.Request)
+	Handler http.Handler
+	After   func(*http.Response, *http.Request)
+}
+
+func (t *HandlerTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	if t.Before != nil {
+		t.Before(r)
+	}
+	w := httptest.NewRecorder()
+	t.Handler.ServeHTTP(w, r)
+	res := w.Result()
+	if t.After != nil {
+		t.After(res, r)
+	}
+	return res, nil
+}
---
+++ transport_test.go
@@ -0,0 +1,33 @@
+package reqtest_test
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/podhmo/reqtest"
+)
+
+func TestHandlerRoundTripper(t *testing.T) {
+	f := reqtest.New(t)
+	f.Client = &http.Client{
+		Transport: &reqtest.HandlerTripper{
+			Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintf(w, "hello world"+r.URL.Query().Get("suffix"))
+			}),
+		},
+	}
+
+	req := f.NewRequest("GET", "http:", nil)
+	q := req.URL.Query()
+	q.Add("suffix", " !!")
+	req.URL.RawQuery = q.Encode()
+
+	res := f.Do(req)
+
+	want := "hello world !!"
+	got := string(f.Extract().Bytes(res))
+	if want != got {
+		t.Errorf("response body\nwant\n\t%+v\nbut\n\t%+v", want, got)
+	}
+}
