8db19ede2cc58ed67ca41ef81bbaf543ac80d89f
podhmo <ababjam61+github@gmail.com>
2021-03-06 08:53:03 +0900

    feat: add DoHandler() and DoHandlerFunc()

--- facade_test.go
+++ facade_test.go
@@ -11,26 +11,53 @@ import (
 	"github.com/podhmo/tenuki"
 )
 
-func TestIt(t *testing.T) {
-	type Body struct {
-		Message string `json:"message"`
-	}
+type Body struct {
+	Message string `json:"message"`
+}
 
-	sumHandler := func(w http.ResponseWriter, r *http.Request) {
-		var xs []int
-		tenuki.DecodeJSON(r.Body, &xs)
-		n := 0
-		for i := range xs {
-			n += xs[i]
-		}
-		tenuki.Render(w, r).JSON(200, Body{Message: fmt.Sprintf("sum is %d", n)})
+func sumHandler(w http.ResponseWriter, r *http.Request) {
+	var xs []int
+	tenuki.DecodeJSON(r.Body, &xs)
+	n := 0
+	for i := range xs {
+		n += xs[i]
 	}
+	tenuki.Render(w, r).JSON(200, Body{Message: fmt.Sprintf("sum is %d", n)})
+}
+
+func TestDo(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(sumHandler))
 	defer ts.Close()
 
 	f := tenuki.New(t)
 	res := f.Do(
 		f.NewRequest("Post", ts.URL, strings.NewReader(`[1,2,3]`)),
+		tenuki.AssertStatus(http.StatusOK),
+	)
+
+	// assertion
+	want := Body{Message: "sum is 6"}
+	var got Body
+	f.Extract().JSON(res, &got)
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("response body\nwant\n\t%+v\nbut\n\t%+v", want, got)
+	}
+
+	// extract multiple times is also ok.
+	{
+		want := Body{Message: "sum is 6"}
+		var got Body
+		f.Extract().JSON(res, &got)
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("response body\nwant\n\t%+v\nbut\n\t%+v", want, got)
+		}
+	}
+}
+
+func TestDoHandler(t *testing.T) {
+	f := tenuki.New(t)
+	res := f.DoHandlerFunc(sumHandler,
+		f.NewRequest("Post", "", strings.NewReader(`[1,2,3]`)),
 		tenuki.AssertStatus(http.StatusOK),
 	)
 
