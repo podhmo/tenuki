a5960bfa71548628d59302b8513810c6c46f9b30
podhmo <ababjam61+github@gmail.com>
2021-03-07 05:38:53 +0900

    feat: until parsing parameters

--- capture/dump_json.go
+++ capture/dump_json.go
@@ -8,10 +8,10 @@ import (
 	"github.com/podhmo/tenuki/capture/httputil"
 )
 
-type JsonDumper struct {
+type JSONDumper struct {
 }
 
-func (d *JsonDumper) DumpRequest(p printer, req *http.Request) (State, error) {
+func (d *JSONDumper) DumpRequest(p printer, req *http.Request) (State, error) {
 	info, err := httputil.DumpRequestJSON(req, true /* body */)
 	if err != nil {
 		return nil, err
@@ -22,12 +22,12 @@ func (d *JsonDumper) DumpRequest(p printer, req *http.Request) (State, error) {
 	enc.SetIndent("", "  ")
 	return nil, enc.Encode(info)
 }
-func (d *JsonDumper) DumpError(p printer, state State, err error) error {
+func (d *JSONDumper) DumpError(p printer, state State, err error) error {
 	p.Printf("\x1b[90merror:\n%+v\x1b[0m", err)
 	return err
 }
 
-func (d *JsonDumper) DumpResponse(p printer, state State, res *http.Response) error {
+func (d *JSONDumper) DumpResponse(p printer, state State, res *http.Response) error {
 	info, err := httputil.DumpResponseJSON(res, true /* body */)
 	if err != nil {
 		return err
@@ -39,4 +39,4 @@ func (d *JsonDumper) DumpResponse(p printer, state State, res *http.Response) er
 	return enc.Encode(info)
 }
 
-var _ Dumper = &JsonDumper{}
+var _ Dumper = &JSONDumper{}
--- capture/httputil/dump_json.go
+++ capture/httputil/dump_json.go
@@ -1,20 +1,52 @@
 package httputil
 
 import (
+	"fmt"
+	"io"
 	"net/http"
+	"strings"
+
+	"github.com/podhmo/tenuki/capture/openapi"
 )
 
-type RequestInfo struct {
-	URI    string
-	Method string
-	Proto  string
-	Body   []byte
+type JSONRequestInfo struct {
+	Method      string `json:"method"`
+	URL         string `json:"url"`
+	HTTPVersion string `json:"httpVersion,omitempty"`
+	HeaderSize  int64  `json:"headerSize,omitempty" default:"-1"`
+	BodySize    int64  `json:"bodySize,omitempty" default:"-1"`
+
+	Paths openapi.Paths `json:"paths"`
 }
 
-func DumpRequestJSON(req *http.Request, body bool) (RequestInfo, error) {
-	var err error
-	var info RequestInfo
+func ExtractJSONRequestInfo(req *http.Request, body io.ReadCloser) (JSONRequestInfo, error) {
+	info := JSONRequestInfo{}
 
+	{
+		reqURI := req.RequestURI
+		if reqURI == "" {
+			reqURI = req.URL.RequestURI()
+		}
+
+		absRequestURI := strings.HasPrefix(req.RequestURI, "http://") || strings.HasPrefix(req.RequestURI, "https://")
+		if !absRequestURI {
+			reqURI = fmt.Sprintf("%s://%s%s", valueOrDefault(req.URL.Scheme, "https"), req.URL.Host, reqURI)
+		}
+		info.URL = reqURI
+	}
+
+	info.Method = valueOrDefault(req.Method, "GET")
+	info.HTTPVersion = fmt.Sprintf("HTTP/%d.%d", req.ProtoMajor, req.ProtoMinor)
+	paths, err := extractOpenAPIPaths(req, body)
+	if err != nil {
+		return info, fmt.Errorf("extract paths, %w", err)
+	}
+	info.Paths = paths
+	return info, nil
+}
+
+func DumpRequestJSON(req *http.Request, body bool) (JSONRequestInfo, error) {
+	var err error
 	save := req.Body
 	{
 		if !body || req.Body == nil {
@@ -22,25 +54,31 @@ func DumpRequestJSON(req *http.Request, body bool) (RequestInfo, error) {
 		} else {
 			save, req.Body, err = drainBody(req.Body)
 			if err != nil {
-				return info, err
+				return JSONRequestInfo{}, err
 			}
 		}
 	}
-
-	_ = save
+	info, err := ExtractJSONRequestInfo(req, save)
+	if err != nil {
+		return JSONRequestInfo{}, err
+	}
 	return info, nil
 }
 
 type ResponseInfo struct {
 }
 
-func DumpResponseJSON(resp *http.Response, body bool) (ResponseInfo, error) {
+func ExtractResponseInfo(resp *http.Response) ResponseInfo {
 	info := ResponseInfo{}
-
+	return info
+}
+func DumpResponseJSON(resp *http.Response, body bool) (ResponseInfo, error) {
+	info := ExtractResponseInfo(resp)
 	var err error
 	save := resp.Body
 	savecl := resp.ContentLength
 
+	// TODO: content-type, json の場合は取り出す
 	{
 		if !body {
 			// For content length of zero. Make sure the body is an empty
