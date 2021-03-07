60a83d2bbde4ea7e824bfbacc0a417ac2b0977f6
podhmo <ababjam61+github@gmail.com>
2021-03-07 11:10:04 +0900

    tests: update transport_test.go

--- transport_test.go
+++ transport_test.go
@@ -1,9 +1,11 @@
 package tenuki_test
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/podhmo/tenuki"
 )
@@ -13,18 +15,34 @@ func TestHandlerRoundTripper(t *testing.T) {
 		fmt.Fprintf(w, "hello world"+r.URL.Query().Get("suffix"))
 	})
 
-	f := tenuki.New(t)
-	req := f.NewRequest("GET", "", nil)
+	req, err := http.NewRequest("GET", "", nil)
+	if err != nil {
+		t.Fatalf("! %+v", err)
+	}
 
 	q := req.URL.Query()
 	q.Add("suffix", " !!")
 	req.URL.RawQuery = q.Encode()
 
-	f.Client = &http.Client{Transport: transport}
-	res := f.Do(req)
+	client := &http.Client{
+		Timeout:   1 * time.Second,
+		Transport: transport,
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("!! %+v", err)
+	}
 
 	want := "hello world !!"
-	got := string(f.Extract().Bytes(res))
+
+	var b bytes.Buffer
+	if _, err := b.ReadFrom(res.Body); err != nil {
+		t.Fatalf("!!! %+v", err)
+	}
+	defer res.Body.Close()
+
+	got := b.String()
 	if want != got {
 		t.Errorf("response body\nwant\n\t%+v\nbut\n\t%+v", want, got)
 	}
