b7ddc9aadb8752dad674555c49308b2a4fb49d17
podhmo <ababjam61+github@gmail.com>
2021-03-10 07:07:13 +0900

    feat: add TimeoutHandler

--- handler.go
+++ handler.go
@@ -3,16 +3,37 @@ package tenuki
 import (
 	"net/http"
 	"testing"
+	"time"
 )
 
 // NewCloseConnectionHandler is a handler that closes the connecion suddenly. this is one of the test utilities.
 func NewCloseConnectionHandler(t *testing.T) http.HandlerFunc {
+	t.Helper()
 	return func(w http.ResponseWriter, r *http.Request) {
 		c, _, err := w.(http.Hijacker).Hijack()
 		if err != nil {
 			t.Fatal(err)
 		}
-		t.Logf("close connection by remote (for %s)", t.Name())
+		t.Logf("test helper -- the connection is closed suddenly by remote side (for %s)", t.Name())
 		c.Close()
 	}
+}
+
+// NewTimeoutHandler is a handler that closes the connecion suddenly. this is one of the test utilities.
+func NewTimeoutHandler(t *testing.T, timeout time.Duration) http.HandlerFunc {
+	t.Helper()
+	return func(w http.ResponseWriter, r *http.Request) {
+		t.Logf("test helper -- the timeout %+v is reached in remote side (for %s) ..", timeout, t.Name())
+		time.Sleep(2 * timeout)
+	}
+}
+
+// NewTimeoutHandlerWithClient is a handler that timeout is occured. this is one of the test utilities.
+func NewTimeoutHandlerWithClient(t *testing.T, timeouts ...time.Duration) (http.HandlerFunc, *http.Client) {
+	t.Helper()
+	timeout := 100 * time.Millisecond
+	if len(timeouts) > 0 {
+		timeout = timeouts[0]
+	}
+	return NewTimeoutHandler(t, timeout), &http.Client{Timeout: timeout}
 }
--- handler_test.go
+++ handler_test.go
@@ -28,3 +28,31 @@ func TestCloseConnectionHandler(t *testing.T) {
 		t.Errorf("EOF is expected but return error is %[1]T, %+[1]v", err)
 	}
 }
+
+func TestTimeoutHandler(t *testing.T) {
+	handler, client := tenuki.NewTimeoutHandlerWithClient(t)
+	ts := httptest.NewServer(handler)
+	defer ts.Close()
+
+	req, err := http.NewRequest("GET", ts.URL, nil)
+	if err != nil {
+		t.Fatalf("unexpected request creation error %#+v", err)
+	}
+
+	_, err = client.Do(req)
+	if err == nil {
+		t.Fatal("must be error, but nil")
+	}
+
+	// &url.Error{Op:"Get", URL:"http://127.0.0.1:59233", Err:(*http.httpError)(0xc0001b2040)}
+	for {
+		inner, ok := err.(interface{ Unwrap() error })
+		if !ok {
+			break
+		}
+		err = inner.Unwrap()
+	}
+	if err, ok := err.(interface{ Timeout() bool }); !(ok && err.Timeout()) {
+		t.Errorf("timeout is expected but return error is %[1]T, %#+[1]v", err)
+	}
+}
