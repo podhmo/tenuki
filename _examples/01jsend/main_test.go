b9f39fe955e10e55ad5b91baea6619239354dabe
podhmo <ababjam61+github@gmail.com>
2021-03-11 07:39:40 +0900

    chore: jsend examples

---
+++ _examples/01jsend/main_test.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/podhmo/tenuki"
+	"github.com/podhmo/tenuki/difftest"
+)
+
+// func init() {
+// 	tenuki.DefaultLayout = capture.OpenAPILayout
+// }
+
+func TestSuccess(t *testing.T) {
+	targetHandler := SuccessHandler
+
+	f := tenuki.New(t)
+	req := f.NewRequest("GET", "", nil)
+	res := f.DoHandlerFunc(targetHandler, req,
+		tenuki.AssertStatus(200),
+	)
+
+	want := `
+{
+    "status" : "success",
+    "data" : {
+        "posts" : [
+            { "id" : 1, "title" : "A blog post", "body" : "Some useful content" },
+            { "id" : 2, "title" : "Another blog post", "body" : "More content" }
+        ]
+     }
+}`
+	got := f.Extract().JSON(res)
+	difftest.AssertGotAndWantString(t, got, want)
+}
+
+func TestFail(t *testing.T) {
+	targetHandler := FailHandler
+
+	f := tenuki.New(t)
+	req := f.NewJSONRequest("POST", "/articles", strings.NewReader(`{"content": "Some useful content"}`))
+	res := f.DoHandlerFunc(targetHandler, req,
+		tenuki.AssertStatus(400),
+	)
+
+	want := `
+{
+    "status" : "fail",
+    "message" : "bad request",
+    "data" : {
+        "title": "A title is required"
+     }
+}`
+	got := f.Extract().JSON(res)
+	difftest.AssertGotAndWantString(t, got, want)
+}
+
+func TestError(t *testing.T) {
+	targetHandler := ErrorHandler
+
+	f := tenuki.New(t)
+	req := f.NewRequest("GET", "", nil)
+	res := f.DoHandlerFunc(targetHandler, req,
+		tenuki.AssertStatus(500),
+	)
+
+	want := `
+{
+    "status" : "error",
+    "message" : "Unable to communicate with database"
+}`
+	got := f.Extract().JSON(res)
+	difftest.AssertGotAndWantString(t, got, want)
+}
+
+func TestNetworkUnreached(t *testing.T) {
+	f := tenuki.New(t)
+	req := f.NewRequest("GET", "xxx://localhost.xxx", nil)
+	f.Do(req,
+		tenuki.AssertError(func(err error) error {
+			if err == nil {
+				return fmt.Errorf("something wrong !!!!!!!!!!!!")
+			}
+			t.Logf("ok error is occured %q", err)
+			return nil
+		}),
+	)
+}
